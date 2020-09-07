0aba1990807c5e81cfafba4a567ee786dcede095
Littlombie <deanwen258@outlook.com>
2020-09-07 18:18:00 +0800

    配置路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   outputDir:process.env.outputDir,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: './',
   chainWebpack: config => {
     if (process.env.use_analyzer) {
       config
